abc89a5ac4d7f4f7afeff9a5a7720da94eb0bf02
EysonB <eysonbasurto@gmail.com>
2026-01-12 20:45:54 -0500

    Ya funciona el agregar medicos ahora vienen las etapas de mejorar un poco la navegacion, testear y demas

--- frontend/src/MedicoForm.jsx
+++ frontend/src/MedicoForm.jsx
@@ -7,26 +7,35 @@ export default function MedicoForm() {
 
   const [nombre, setNombre] = useState("");
   const [apellido, setApellido] = useState("");
+  const [cedula, setCedula] = useState("");
   const [especialidad, setEspecialidad] = useState("");
   const [telefono, setTelefono] = useState("");
   const [email, setEmail] = useState("");
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await axios.post("http://127.0.0.1:8000/api/medicos/", {
-        nombre,
-        apellido,
-        especialidad,
-        telefono,
-        email,
-      });
-      navigate("/medicos");
-    } catch (err) {
-      console.error(err);
-      alert("Error al crear médico");
-    }
-  };
+  e.preventDefault();
+
+  try {
+    await axios.post("http://127.0.0.1:8000/api/medicos/", {
+      nombre,
+      apellido,
+      cedula,
+      especialidad,
+      telefono: Number(telefono),
+      email,
+    }, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    navigate("/medicos");
+
+  } catch (err) {
+    console.error("ERROR BACKEND:", err.response?.data);
+    alert(JSON.stringify(err.response?.data, null, 2));
+  }
+};
 
   return (
     <div>
@@ -34,6 +43,7 @@ export default function MedicoForm() {
       <form onSubmit={handleSubmit}>
         <input placeholder="Nombre" value={nombre} onChange={e => setNombre(e.target.value)} />
         <input placeholder="Apellido" value={apellido} onChange={e => setApellido(e.target.value)} />
+        <input placeholder="Cedula" value={cedula} onChange={e => setCedula(e.target.value)} />
         <input placeholder="Especialidad" value={especialidad} onChange={e => setEspecialidad(e.target.value)} />
         <input placeholder="Teléfono" value={telefono} onChange={e => setTelefono(e.target.value)} />
         <input placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
